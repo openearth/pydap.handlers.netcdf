9d3a8cd81995223070776b0fc6493ac73a258138
Roberto De Almeida <roberto@dealmeida.net>
2013-06-24 16:43:43 -0700

    New version for Pydap 3.2

--- src/pydap/handlers/netcdf/__init__.py
+++ src/pydap/handlers/netcdf/__init__.py
@@ -3,155 +3,94 @@ import re
 import time
 from stat import ST_MTIME
 from email.utils import formatdate
-import urllib
 
-import numpy
+import numpy as np
+from pkg_resources import get_distribution
 
-from arrayterator import Arrayterator
+from pupynere import netcdf_file
 
 from pydap.model import *
-from pydap.lib import quote
 from pydap.handlers.lib import BaseHandler
 from pydap.exceptions import OpenFileError
 
-try:
-    from nio import open_file as nc
-    extensions = re.compile(
-            r"^.*\.(nc|cdf|netcdf|grb|grib|grb1|grib1|grb2|grib2|hd|hdf|he2|he4|hdfeos|ccm)$",
-            re.IGNORECASE)
-    var_attrs = lambda var: var.__dict__.copy()
-    get_value = lambda var: var.get_value()
-    get_typecode = lambda var: var.typecode()
-except ImportError:
-    try:
-        from netCDF4 import Dataset as nc
-        extensions = re.compile(
-                r"^.*\.(nc|nc4|cdf|netcdf)$",
-                re.IGNORECASE)
-        var_attrs = lambda var: dict( (a, getattr(var, a))
-                for a in var.ncattrs() )
-        get_value = lambda var: var.getValue()
-        get_typecode = lambda var: var.dtype.char
-    except ImportError:
-        try:
-            from Scientific.IO.NetCDF import NetCDFFile as nc
-            extensions = re.compile(
-                    r"^.*\.(nc|cdf|netcdf)$",
-                    re.IGNORECASE)
-            var_attrs = lambda var: var.__dict__.copy()
-            get_value = lambda var: var.getValue()
-            get_typecode = lambda var: var.typecode()
-        except ImportError:
-            try:
-                from pynetcdf import NetCDFFile as nc
-                extensions = re.compile(
-                        r"^.*\.(nc|cdf|netcdf)$",
-                        re.IGNORECASE)
-                var_attrs = lambda var: var.__dict__.copy()
-                get_value = lambda var: var.getValue()
-                get_typecode = lambda var: var.typecode()
-            except ImportError:
-                from pupynere import NetCDFFile as nc
-                extensions = re.compile(
-                        r"^.*\.(nc|cdf|netcdf)$",
-                        re.IGNORECASE)
-                var_attrs = lambda var: var._attributes.copy()
-                get_value = lambda var: var.getValue()
-                get_typecode = lambda var: var.typecode()
 
+class NetCDFHandler(BaseHandler):
 
-class Handler(BaseHandler):
-
-    extensions = extensions
+    __version__ = get_distribution("pydap.handlers.netcdf").version
+    extensions = re.compile(r"^.*\.(nc|cdf)$", re.IGNORECASE)
 
     def __init__(self, filepath):
-        self.filepath = filepath
-
-    def parse_constraints(self, environ):
-        buf_size = int(environ.get('pydap.handlers.netcdf.buf_size', 10000))
+        BaseHandler.__init__(self)
 
         try:
-            fp = nc(self.filepath)
-        except:
-            message = 'Unable to open file %s.' % self.filepath
+            self.fp = netcdf_file(filepath)
+        except Exception, exc:
+            message = 'Unable to open file %s: %s' % (filepath, exc)
             raise OpenFileError(message)
 
-        last_modified = formatdate( time.mktime( time.localtime( os.stat(self.filepath)[ST_MTIME] ) ) )
-        environ['pydap.headers'].append( ('Last-modified', last_modified) )
+        self.additional_headers.append(
+                ('Last-modified', (formatdate(time.mktime(time.localtime(os.stat(filepath)[ST_MTIME]))))))
 
-        dataset = DatasetType(name=os.path.split(self.filepath)[1],
-                attributes={'NC_GLOBAL': var_attrs(fp)})
-        for dim in fp.dimensions:
-            if fp.dimensions[dim] is None:
-                dataset.attributes['DODS_EXTRA'] = {'Unlimited_Dimension': dim}
+        # shortcuts
+        vars = self.fp.variables
+        dims = self.fp.dimensions
+
+        # build dataset
+        name = os.path.split(filepath)[1]
+        self.dataset = DatasetType(name, attributes=dict(NC_GLOBAL=self.fp._attributes))
+        for dim in dims:
+            if dims[dim] is None:
+                self.dataset.attributes['DODS_EXTRA'] = {'Unlimited_Dimension': dim}
                 break
 
-        fields, queries = environ['pydap.ce']
-        fields = fields or [[(quote(name), ())] for name in fp.variables]
-        for var in fields:
-            target = dataset
-            while var:
-                name, slice_ = var.pop(0)
-                ncname = urllib.unquote(name)
-                if (ncname in fp.dimensions or
-                        not fp.variables[ncname].dimensions or
-                        target is not dataset):
-                    target[name] = get_var(ncname, fp, slice_, buf_size)
-                elif var:
-                    attrs = var_attrs(fp.variables[ncname])
-                    target.setdefault(name, StructureType(name=name, attributes=attrs))
-                    target = target[name]
-                else:  # return grid
-                    attrs = var_attrs(fp.variables[ncname])
-                    grid = target[name] = GridType(name=name, attributes=attrs)
-                    grid[name] = get_var(ncname, fp, slice_, buf_size)
-                    slice_ = list(slice_) + [slice(None)] * (len(grid.array.shape) - len(slice_))
-                    for dim, dimslice in zip(fp.variables[ncname].dimensions, slice_):
-                        axis = get_var(dim, fp, dimslice, buf_size)
-                        grid[axis.name] = axis
+        # add grids
+        grids = [var for var in vars if var not in dims]
+        for grid in grids:
+            self.dataset[grid] = GridType(grid, vars[grid]._attributes)
+            # add array
+            self.dataset[grid][grid] = BaseType(grid, NetcdfData(vars[grid]),
+                    vars[grid].dimensions, vars[grid]._attributes)
+            # add maps
+            for dim in vars[grid].dimensions:
+                self.dataset[grid][dim] = BaseType(dim, vars[dim][:],
+                        None, vars[dim]._attributes)
 
-        dataset._set_id()
-        dataset.close = fp.close
-        return dataset
+        # add dims
+        for dim in dims:
+            self.dataset[dim] = BaseType(dim, vars[dim][:],
+                    None, vars[dim]._attributes)
+
+    def close(self):
+        self.fp.close()
 
 
-def get_var(name, fp, slice_, buf_size=10000):
-    if name in fp.variables:
-        var = fp.variables[name]
-        if hasattr(var, 'set_auto_maskandscale'):
-            var.set_auto_maskandscale(False)
-        if var.shape: 
-            data = Arrayterator(var, buf_size)[slice_]
-        else:
-            data = numpy.array(get_value(var))
-        typecode = get_typecode(var)
-        dims = tuple(quote(dim) for dim in var.dimensions)
-        attrs = var_attrs(var)
-    else:
-        for var in fp.variables:
-            var = fp.variables[var]
-            if name in var.dimensions:
-                size = var.shape[
-                        list(var.dimensions).index(name)]
-                break
-        data = numpy.arange(size)[slice_]
-        typecode = data.dtype.char
-        dims, attrs = (quote(name),), {}
+class NetcdfData(object):
+    """
+    A wrapper for Netcdf variables, making them behave more like Numpy arrays.
 
-    # handle char vars
-    if typecode == 'S1':
-        typecode = 'S'
-        data = numpy.array([''.join(row) for row in numpy.asarray(data)])
-        dims = dims[:-1]
+    """
+    def __init__(self, var):
+        self.var = var
+        self.dtype = np.dtype(self.var.typecode())
+        self.shape = var.shape
 
-    return BaseType(name=name, data=data, shape=data.shape,
-            type=typecode, dimensions=dims,
-            attributes=attrs)
+    # Comparisons are passed to the data.
+    def __eq__(self, other): return self.var[:] == other
+    def __ne__(self, other): return self.var[:] != other
+    def __ge__(self, other): return self.var[:] >= other
+    def __le__(self, other): return self.var[:] <= other
+    def __gt__(self, other): return self.var[:] > other
+    def __lt__(self, other): return self.var[:] < other
+
+    # Implement the sequence and iter protocols.
+    def __getitem__(self, index): return self.var[index]
+    def __len__(self): return self.shape[0]
+    def __iter__(self): return iter(self.var[:])    
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    from paste.httpserver import serve
+    from werkzeug.serving import run_simple
 
-    application = Handler(sys.argv[1])
-    serve(application, port=8001)
+    application = NetCDFHandler(sys.argv[1])
+    run_simple('localhost', 8001, application, use_reloader=True)
