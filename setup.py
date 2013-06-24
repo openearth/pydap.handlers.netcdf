d535d2718982f9d46f2de0a4a4abd7d03746a9c0
Roberto De Almeida <roberto@dealmeida.net>
2013-06-24 16:45:06 -0700

    Add build files

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import sys, os
+
+
+version = '0.7'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+
+setup(name='pydap.handlers.netcdf',
+    version=version,
+    description="Netcdf handler for Pydap",
+    long_description="",
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='netcdf opendap dods dap science meteorology oceanography',
+    author='Roberto De Almeida',
+    author_email='roberto@dealmeida.net',
+    url='http://pydap.org/handlers.html#netcdf',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages = ['pydap', 'pydap.handlers'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points="""
+        [pydap.handler]
+        netcdf = pydap.handlers.netcdf:NetCDFHandler
+    """,
+)
