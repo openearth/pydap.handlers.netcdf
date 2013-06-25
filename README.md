d669a79e08f30b1e3723c60596ae58e7b734f2f5
Roberto De Almeida <roberto@dealmeida.net>
2013-06-25 14:10:17 -0700

    Updated documentation

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 pydap.handlers.netcdf
 =====================
 
-This handler allows Pydap to serve data from NetCDF files.
+This handler allows Pydap to serve data from NetCDF files. By default 
+[pupynere](http://code.dealmeida.net/pupynere) is installed as a dependency,
+providing support for NetCDF 3 files. If you also want to server NetCDF 4 files
+you'll need to install the [netCDF4-python](https://code.google.com/p/netcdf4-python/)
+library.
--- setup.py
+++ setup.py
@@ -16,14 +16,20 @@ install_requires = [
 setup(name='pydap.handlers.netcdf',
     version=version,
     description="Netcdf handler for Pydap",
-    long_description="",
+    long_description="""
+This handler allows Pydap to serve data from NetCDF files. By default           
+`pupynere <http://code.dealmeida.net/pupynere>`_ is installed as a dependency,    
+providing support for NetCDF 3 files. If you also want to server NetCDF 4 files 
+you'll need to install the `netCDF4-python <https://code.google.com/p/netcdf4-python/>`_
+library.
+""",
     classifiers=[
       # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     ],
     keywords='netcdf opendap dods dap science meteorology oceanography',
     author='Roberto De Almeida',
     author_email='roberto@dealmeida.net',
-    url='http://pydap.org/handlers.html#netcdf',
+    url='https://github.com/robertodealmeida/pydap.handlers.netcdf',
     license='MIT',
     packages=find_packages('src'),
     package_dir = {'': 'src'},
