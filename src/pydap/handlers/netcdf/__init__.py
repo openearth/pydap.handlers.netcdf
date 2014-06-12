bf0d1a4b992e045063c55255c26e91d6724401ef
Fedor Baart <fedor.baart@deltares.nl>
2014-06-12 11:18:13 +0200

    read sequences

--- src/pydap/handlers/netcdf/__init__.py
+++ src/pydap/handlers/netcdf/__init__.py
@@ -7,6 +7,7 @@ from stat import ST_MTIME
 from email.utils import formatdate
 
 import numpy as np
+
 from pkg_resources import get_distribution
 
 # available NetCDF modules
@@ -23,18 +24,32 @@ try:
             return {k: getattr(var, k) for k in var.ncattrs()}
         else:
             return {}
-
+    import netCDF4
+    def chartostring(ds, name):
+        """convert char array to string from the dataset"""
+        var = netCDF4.chartostring(ds.variables[name][:])
+        return var
 except ImportError:
     from pupynere import netcdf_file
     NETCDF_MODULE = PUPYNERE
-    
+
     def attrs(var):
         """Pupynere stores attributes in a special attribute."""
         return getattr(var, "_attributes", {})
+    def chartostring(ds, name):
+        """convert char array to string from the dataset"""
+        # assume final dimension is string length
+        charlen = ds.dimensions[ds.variables[name].dimensions[-1]]
+        # create a string type that matches the character
+        type_ = 'S%d' % (charlen, )
+        # view as string and copy
+        arr = ds.variables[name][:].view(type_).copy()
+        return arr
 
 from pydap.model import *
 from pydap.handlers.lib import BaseHandler
 from pydap.exceptions import OpenFileError
+
 
 
 class NetCDFHandler(BaseHandler):
@@ -88,8 +103,57 @@ class NetCDFHandler(BaseHandler):
                 }
                 break
 
+        # create sequence types (tables)
+        oned_string_vars = [
+            var
+            for var in vars
+            if vars[var].dtype.char == 'S'
+            and len(vars[var].dimensions) == 2
+        ]
+        oned_scalar_vars = [
+            var
+            for var in vars
+            if len(vars[var].dimensions) == 1
+        ]
+
+        oned_vars = set(oned_vars) | set(oned_string_vars)
+
+        # group variables by sequences (sequences have shared first dimension)
+        sequences = {}
+        for var in oned_vars:
+            key = vars[var].dimensions[0]
+            sequences[key] = sequences.get(key, [])  + [var]
+
+        # read data as a recarray and embed in SequenceType
+        for seq in sequences:
+            # read all data as vectors
+            vecs = {}
+            for var in sequences[seq]:
+                # convert chararray to string
+                if vars[var].dtype.char == 'S':
+                    vec = chartostring(vars[var][:])
+                else:
+                    vec = vars[var][:]
+                vecs[var] = vec
+            # create a np.rec.array
+            dtype = [(bytes(var), str(vecs[var].dtype)) for var in vecs]
+            arr = np.recarray(
+                shape=(len(dims[seq]),),
+                dtype=dtype
+            )
+            # with data
+            for var in vecs:
+                arr[var][:] = vecs[var]
+            # create a pydap model based on the data
+            seqtype = SequenceType(name=seq, data=arr)
+            for var in vecs:
+                seqtype[var] = BaseType(name=var)
+                seqtype[var].data = vecs[var]
+            self.dataset[seq] = seqtype
+
+
         # add grids
-        grids = [var for var in vars if var not in dims]
+        grids = [var for var in vars if var not in dims and var not in oned_vars]
         for grid in grids:
             self.dataset[grid] = GridType(grid, attrs(vars[grid]))
             # add array
@@ -97,13 +161,18 @@ class NetCDFHandler(BaseHandler):
                 grid, vars[grid], vars[grid].dimensions, attrs(vars[grid]))
             # add maps
             for dim in vars[grid].dimensions:
-                self.dataset[grid][dim] = BaseType(
-                    dim, vars[dim][:], None, attrs(vars[dim]))
+                if dim in vars:
+                    self.dataset[grid][dim] = BaseType(
+                        dim, vars[dim][:], None, attrs(vars[dim]))
 
         # add dims
         for dim in dims:
-            self.dataset[dim] = BaseType(
-                dim, vars[dim][:], None, attrs(vars[dim]))
+            # some vars are already in scalars
+            if dim in vars:
+                self.dataset[dim] = BaseType(
+                    dim, vars[dim][:], None, attrs(vars[dim]))
+            # what about the other dims? the other variables that are not named after their dimension?
+
 
     def close(self):
         """Close the NetCDF file."""
